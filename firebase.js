91b3bf7146c52d8472db2fa56443dc5f0bc9d822
martinsdh <cuhelmartin2@gmail.com>
2026-06-16 13:58:56 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,17 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDJf1nlJOqkK1SLfxu4836RhV4VURejFgE",
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "TVŮJ_API_KEY",
   authDomain: "evidence-sdh-opatov.firebaseapp.com",
   projectId: "evidence-sdh-opatov",
   storageBucket: "evidence-sdh-opatov.firebasestorage.app",
   messagingSenderId: "910376724668",
   appId: "1:910376724668:web:37e8b08c3fa6e01d6b08b9"
 };
+
+const app = initializeApp(firebaseConfig);
+
+window.auth = getAuth(app);
+window.db = getFirestore(app);
