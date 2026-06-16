c84829b83ffc789851163c9a86b7ee54bd354a53
martinsdh <cuhelmartin2@gmail.com>
2026-06-16 13:55:21 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDJf1nlJOqkK1SLfxu4836RhV4VURejFgE",
+  authDomain: "evidence-sdh-opatov.firebaseapp.com",
+  projectId: "evidence-sdh-opatov",
+  storageBucket: "evidence-sdh-opatov.firebasestorage.app",
+  messagingSenderId: "910376724668",
+  appId: "1:910376724668:web:37e8b08c3fa6e01d6b08b9"
+};
